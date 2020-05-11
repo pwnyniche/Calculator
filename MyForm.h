97bb266cc8f1ac1cca45fc734a82b5764186ae30
tqtnk2000 <62285621+tqtnk2000@users.noreply.github.com>
2020-05-11 17:59:08 +0700

    Add files via upload

--- MyForm.h
+++ MyForm.h
@@ -34,6 +34,33 @@ namespace Calculator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button8;
+	private: System::Windows::Forms::Button^ button9;
+	private: System::Windows::Forms::Button^ button10;
+	private: System::Windows::Forms::Button^ button11;
+	private: System::Windows::Forms::Button^ button12;
+	private: System::Windows::Forms::Button^ button13;
+	private: System::Windows::Forms::Button^ button14;
+	private: System::Windows::Forms::Button^ button15;
+	private: System::Windows::Forms::Button^ button16;
+	private: System::Windows::Forms::Button^ button17;
+	private: System::Windows::Forms::Button^ button18;
+	private: System::Windows::Forms::Button^ button19;
+	private: System::Windows::Forms::Button^ button20;
+	private: System::Windows::Forms::Button^ button21;
+	private: System::Windows::Forms::Button^ button22;
+	private: System::Windows::Forms::Button^ button23;
+	private: System::Windows::Forms::Button^ button24;
+	private: System::Windows::Forms::Button^ button25;
 
 	private:
 		/// <summary>
@@ -48,12 +75,364 @@ namespace Calculator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->button10 = (gcnew System::Windows::Forms::Button());
+			this->button11 = (gcnew System::Windows::Forms::Button());
+			this->button12 = (gcnew System::Windows::Forms::Button());
+			this->button13 = (gcnew System::Windows::Forms::Button());
+			this->button14 = (gcnew System::Windows::Forms::Button());
+			this->button15 = (gcnew System::Windows::Forms::Button());
+			this->button16 = (gcnew System::Windows::Forms::Button());
+			this->button17 = (gcnew System::Windows::Forms::Button());
+			this->button18 = (gcnew System::Windows::Forms::Button());
+			this->button19 = (gcnew System::Windows::Forms::Button());
+			this->button20 = (gcnew System::Windows::Forms::Button());
+			this->button21 = (gcnew System::Windows::Forms::Button());
+			this->button22 = (gcnew System::Windows::Forms::Button());
+			this->button23 = (gcnew System::Windows::Forms::Button());
+			this->button24 = (gcnew System::Windows::Forms::Button());
+			this->button25 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Arial", 19.875F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(12, 60);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(736, 104);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"0";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleRight;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(171, 320);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(128, 125);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->Location = System::Drawing::Point(321, 320);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(128, 125);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->Location = System::Drawing::Point(473, 320);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(128, 125);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->Location = System::Drawing::Point(620, 320);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(128, 125);
+			this->button4->TabIndex = 4;
+			this->button4->Text = L"+";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// button5
+			// 
+			this->button5->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->Location = System::Drawing::Point(620, 473);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(128, 125);
+			this->button5->TabIndex = 8;
+			this->button5->Text = L"-";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button6->Location = System::Drawing::Point(473, 473);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(128, 125);
+			this->button6->TabIndex = 7;
+			this->button6->Text = L"6";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button7->Location = System::Drawing::Point(321, 473);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(128, 125);
+			this->button7->TabIndex = 6;
+			this->button7->Text = L"5";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button8->Location = System::Drawing::Point(171, 473);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(128, 125);
+			this->button8->TabIndex = 5;
+			this->button8->Text = L"4";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this->button9->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button9->Location = System::Drawing::Point(620, 623);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(128, 125);
+			this->button9->TabIndex = 12;
+			this->button9->Text = L"*";
+			this->button9->UseVisualStyleBackColor = true;
+			// 
+			// button10
+			// 
+			this->button10->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button10->Location = System::Drawing::Point(473, 623);
+			this->button10->Name = L"button10";
+			this->button10->Size = System::Drawing::Size(128, 125);
+			this->button10->TabIndex = 11;
+			this->button10->Text = L"9";
+			this->button10->UseVisualStyleBackColor = true;
+			// 
+			// button11
+			// 
+			this->button11->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button11->Location = System::Drawing::Point(321, 623);
+			this->button11->Name = L"button11";
+			this->button11->Size = System::Drawing::Size(128, 125);
+			this->button11->TabIndex = 10;
+			this->button11->Text = L"8";
+			this->button11->UseVisualStyleBackColor = true;
+			// 
+			// button12
+			// 
+			this->button12->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button12->Location = System::Drawing::Point(171, 623);
+			this->button12->Name = L"button12";
+			this->button12->Size = System::Drawing::Size(128, 125);
+			this->button12->TabIndex = 9;
+			this->button12->Text = L"7";
+			this->button12->UseVisualStyleBackColor = true;
+			// 
+			// button13
+			// 
+			this->button13->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button13->Location = System::Drawing::Point(620, 776);
+			this->button13->Name = L"button13";
+			this->button13->Size = System::Drawing::Size(128, 125);
+			this->button13->TabIndex = 16;
+			this->button13->Text = L"/";
+			this->button13->UseVisualStyleBackColor = true;
+			// 
+			// button14
+			// 
+			this->button14->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button14->Location = System::Drawing::Point(473, 776);
+			this->button14->Name = L"button14";
+			this->button14->Size = System::Drawing::Size(128, 125);
+			this->button14->TabIndex = 15;
+			this->button14->Text = L"=";
+			this->button14->UseVisualStyleBackColor = true;
+			// 
+			// button15
+			// 
+			this->button15->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button15->Location = System::Drawing::Point(321, 776);
+			this->button15->Name = L"button15";
+			this->button15->Size = System::Drawing::Size(128, 125);
+			this->button15->TabIndex = 14;
+			this->button15->Text = L".";
+			this->button15->UseVisualStyleBackColor = true;
+			// 
+			// button16
+			// 
+			this->button16->Font = (gcnew System::Drawing::Font(L"Arial", 22.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button16->Location = System::Drawing::Point(171, 776);
+			this->button16->Name = L"button16";
+			this->button16->Size = System::Drawing::Size(128, 125);
+			this->button16->TabIndex = 13;
+			this->button16->Text = L"0";
+			this->button16->UseVisualStyleBackColor = true;
+			// 
+			// button17
+			// 
+			this->button17->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button17->Location = System::Drawing::Point(620, 178);
+			this->button17->Name = L"button17";
+			this->button17->Size = System::Drawing::Size(128, 125);
+			this->button17->TabIndex = 20;
+			this->button17->Text = L"AC";
+			this->button17->UseVisualStyleBackColor = true;
+			// 
+			// button18
+			// 
+			this->button18->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button18->Location = System::Drawing::Point(473, 178);
+			this->button18->Name = L"button18";
+			this->button18->Size = System::Drawing::Size(128, 125);
+			this->button18->TabIndex = 19;
+			this->button18->Text = L"C";
+			this->button18->UseVisualStyleBackColor = true;
+			// 
+			// button19
+			// 
+			this->button19->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button19->Location = System::Drawing::Point(321, 178);
+			this->button19->Name = L"button19";
+			this->button19->Size = System::Drawing::Size(128, 125);
+			this->button19->TabIndex = 18;
+			this->button19->Text = L"HEX";
+			this->button19->UseVisualStyleBackColor = true;
+			// 
+			// button20
+			// 
+			this->button20->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button20->Location = System::Drawing::Point(171, 178);
+			this->button20->Name = L"button20";
+			this->button20->Size = System::Drawing::Size(128, 125);
+			this->button20->TabIndex = 17;
+			this->button20->Text = L"DEC";
+			this->button20->UseVisualStyleBackColor = true;
+			// 
+			// button21
+			// 
+			this->button21->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button21->Location = System::Drawing::Point(24, 178);
+			this->button21->Name = L"button21";
+			this->button21->Size = System::Drawing::Size(128, 125);
+			this->button21->TabIndex = 25;
+			this->button21->Text = L"BIN";
+			this->button21->UseVisualStyleBackColor = true;
+			// 
+			// button22
+			// 
+			this->button22->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button22->Location = System::Drawing::Point(24, 776);
+			this->button22->Name = L"button22";
+			this->button22->Size = System::Drawing::Size(128, 125);
+			this->button22->TabIndex = 24;
+			this->button22->Text = L"NOT";
+			this->button22->UseVisualStyleBackColor = true;
+			// 
+			// button23
+			// 
+			this->button23->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button23->Location = System::Drawing::Point(24, 623);
+			this->button23->Name = L"button23";
+			this->button23->Size = System::Drawing::Size(128, 125);
+			this->button23->TabIndex = 23;
+			this->button23->Text = L"XOR";
+			this->button23->UseVisualStyleBackColor = true;
+			// 
+			// button24
+			// 
+			this->button24->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button24->Location = System::Drawing::Point(24, 473);
+			this->button24->Name = L"button24";
+			this->button24->Size = System::Drawing::Size(128, 125);
+			this->button24->TabIndex = 22;
+			this->button24->Text = L"OR";
+			this->button24->UseVisualStyleBackColor = true;
+			// 
+			// button25
+			// 
+			this->button25->Font = (gcnew System::Drawing::Font(L"Arial", 16.125F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button25->Location = System::Drawing::Point(24, 320);
+			this->button25->Name = L"button25";
+			this->button25->Size = System::Drawing::Size(128, 125);
+			this->button25->TabIndex = 21;
+			this->button25->Text = L"AND";
+			this->button25->UseVisualStyleBackColor = true;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(760, 913);
+			this->Controls->Add(this->button21);
+			this->Controls->Add(this->button22);
+			this->Controls->Add(this->button23);
+			this->Controls->Add(this->button24);
+			this->Controls->Add(this->button25);
+			this->Controls->Add(this->button17);
+			this->Controls->Add(this->button18);
+			this->Controls->Add(this->button19);
+			this->Controls->Add(this->button20);
+			this->Controls->Add(this->button13);
+			this->Controls->Add(this->button14);
+			this->Controls->Add(this->button15);
+			this->Controls->Add(this->button16);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button10);
+			this->Controls->Add(this->button11);
+			this->Controls->Add(this->button12);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
